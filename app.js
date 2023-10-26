44ff961a9cc9e603c97a750afcee055e7d76c95b
Isaac Wilson <iswilson.dev@gmail.com>
2023-10-26 12:25:14 +0000

    feat: initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+
+const argv = require("yargs");
+const sharp = require("sharp");
+const imagemin = require("imagemin")
+const imageminMozjpeg = require("imagemin-mozjpeg")
+const imageminPngquant = require("imagemin-pngquant")
+
+
+
+
