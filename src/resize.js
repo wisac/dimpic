822cf6f282200917c1bf277d48b9f50429103ab8
Isaac Wilson <iswilson.dev@gmail.com>
2023-10-26 19:48:42 +0000

    fix: handle empty file

--- src/resize.js
+++ src/resize.js
@@ -1,23 +1,27 @@
 import sharp from "sharp";
 import printUsage from "./printUsage.js";
 
+
+
 export default function resize(inputFile, width, height) {
-   //get given dimensions
-   const size = width && height ? [width, height] : [width];
 
-   // check if file is given
-   if (!inputFile) {
-      printUsage();
-      throw new Error("Missing input file");
-   }
+      //get given dimensions
+      const size = width && height ? [width, height] : [width];
 
-   // if no dimensions are given, return original file
-   if (!width && !height) {
-      return sharp(inputFile).toBuffer();
-   }
+      // check if file is given
+      if (!inputFile) {
+         printUsage();
+         // throw new Error("Missing input file");
+      }
 
-   // resize and return buffer
+      // if no dimensions are given, return original file
+      if (!width && !height) {
+         return sharp(inputFile).toBuffer();
+      }
+
+      console.log("Resizing...");
+      // resize and return buffer
    return sharp(inputFile)
       .resize(...size)
-      .toBuffer();
-}
+      .toBuffer()
+   }
