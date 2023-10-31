81d2eb586146a8682f1ec81bc07b0fa61307c3dc
Isaac Wilson <iswilson.dev@gmail.com>
2023-10-31 16:32:10 +0000

    refactor: combine properties into a sub-object

--- src/inputHandler.js
+++ src/inputHandler.js
@@ -3,61 +3,80 @@ import yargs from "yargs";
 
 const argv = yargs(process.argv.slice(2)).argv;
 
-const { q, quality, d, dest, w, width, h, height } = argv;
 const currentPath = argv.$0;
 const files = argv._;
+const { q, quality, d, dest, w, width, h, height } = argv;
+
+console.log(argv);
+function validFlags() {
+   return {
+      quality: quality !== undefined ? quality : q,
+      dest: dest !== undefined ? dest : d,
+      width: width !== undefined ? width : w,
+      height: height !== undefined ? height : h,
+   };
+}
 
 const inputInfo = {
-   q,
-   quality,
-   d,
-   dest,
-   w,
-   width,
-   h,
-   height,
-   files,
-   currentPath,
+   _flags: Object.create(validFlags()),
+
+   _files: files,
    currentDir: path.resolve(path.dirname(currentPath)), //async
    destDir: path.resolve(path.dirname(currentPath)),
    fileName:
       path.basename(currentPath, path.extname(currentPath)) +
       "_dim" +
       path.extname(currentPath),
-   // defaultFileName: setDefaultFileName(),
 
-   //set destination direction of output file no filenames
+   //set destination directory
    setDestDir() {
       console.log("dirname setter");
-      if (d)
-         if (path.extname(path.resolve(d))) {
-            this.destDir = path.resolve(path.dirname(d));
+      if (this._flags.dest)
+         if (path.extname(path.resolve(this._flags.dest))) {
+            this.destDir = path.resolve(path.dirname(this._flags.dest));
             console.log("setting dir with extension");
          } else {
-            this.destDir = path.resolve(d);
+            this.destDir = path.resolve(this._flags.dest);
          }
    },
 
-   // sets file name and extension dir excluded
+   // sets output filename
    setFileName() {
       console.log("filename setter");
 
-      if (d) {
-         if (path.extname(path.resolve(d))) {
-            this.fileName = path.basename(path.resolve(d));
+      if (this._flags.dest) {
+         if (path.extname(path.resolve(this._flags.dest))) {
+            this.fileName = path.basename(path.resolve(this._flags.dest));
             console.log("here");
          }
       }
    },
 
+   //get output filename
    getFileName() {
       this.setFileName();
+      console.log("height", this.h, this.height);
       return this.fileName;
    },
 
+   //get output destination
    getDest() {
       this.setDestDir();
       return this.destDir;
+   },
+
+   getFiles() {
+      return this._files;
+   },
+
+   getQuality() {
+      return this._flags.quality;
+   },
+   getWidth() {
+      return this._flags.width;
+   },
+   getHeight() {
+      return this._flags.height;
    },
 };
 
@@ -65,8 +84,6 @@ console.log("destination dir = ", inputInfo.getDest());
 console.log("current dir =", inputInfo.currentDir);
 console.log("filename = ", inputInfo.fileName);
 console.log("gettingFilename = ", inputInfo.getFileName());
-
-// console.log(argv);
 
 //to specify a manual filename, use -d destination/filename
 
@@ -76,4 +93,4 @@ console.log("gettingFilename = ", inputInfo.getFileName());
 
 // how to get determine values to be passed. if
 
-export default  inputInfo ;
+export default inputInfo;
