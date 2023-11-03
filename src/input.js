f4c98989bcee5a49bc8b707f4f9d58d703145ffd
Isaac Wilson <iswilson.dev@gmail.com>
2023-11-03 20:51:22 +0000

    feat: implement filename handler

---
+++ src/input.js
@@ -0,0 +1,77 @@
+import { Command } from "commander";
+import path from "path";
+
+// create a command instant
+const program = new Command();
+
+function getName(file) {
+   //total files to work on
+   const totalFiles = program.args.length;
+
+   //default file location
+   let fileLoc = path.resolve(path.dirname(file));
+
+   //default filename
+   let filename =
+      path.basename(path.resolve(file), path.extname(path.resolve(file))) +
+      "_dim" +
+      path.extname(path.resolve(file));
+
+   // manual filename
+   if (program.opts().dest) {
+
+      const destination = program.opts().dest;
+
+      if (path.extname(path.resolve(destination))) {
+         //if extension is provided
+         filename =
+            totalFiles > 1
+               ? filename
+               : path.basename(path.resolve(destination));
+         
+         fileLoc = path.resolve(path.dirname(destination));
+
+      } else {
+         fileLoc = path.resolve(destination);
+      }
+   }
+
+   const fileDestination = fileLoc + path.sep + filename;
+
+   return fileDestination;
+}
+
+
+function inputHandler() {
+   const files = program.args
+   const options = program.opts();
+
+   //if no input given
+   if (files.length < 1) {
+      //call usage
+      process.exit
+   }
+
+   // if no options are provided (flags)
+   if (Object.keys(options).length < 1) {
+      //compress image with default options
+   }
+
+   
+
+}
+
+// program.command()
+
+program.option("-d, --dest <path>", "Output of file");
+program.option("-h, --height <number>", "height of image");
+
+program.parse();
+// const loc =program.nameFromFilename(process.argv[1])
+
+// program.args.forEach((el) => {
+//    console.log("output", getName(el));
+// });
+
+// console.log(program.args);
+console.log(program.opts());
